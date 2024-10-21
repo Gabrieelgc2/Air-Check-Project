f235f2ed9443b187d832e35bb35690eec124e848
Gabrieelgc2 <gabrielao8@hotmail.com>
2024-10-21 11:10:51 -0300

    New update

--- air_check_project/app_aircheck_project/models.py
+++ air_check_project/app_aircheck_project/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Usuarios(models.Model):
+    id_usuario = models.AutoField(primary_key = True)
+    name = models.TextField(max_length = 255)
+    email = models.TextField(max_length = 255)
+    subject = models.TextField(max_length = 255)
+    message = models.TextField(max_length = 255)
+    
